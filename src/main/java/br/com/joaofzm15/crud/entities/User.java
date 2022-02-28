3d8d5a5abba5e9387523672630919df0dbdc0ff1
unknown <joao.fzm@hotmail.com>
2022-02-28 01:03:36 -0300

    Create H2 test database

--- src/main/java/br/com/joaofzm15/crud/entities/User.java
+++ src/main/java/br/com/joaofzm15/crud/entities/User.java
@@ -3,10 +3,21 @@ package br.com.joaofzm15.crud.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	//Id to mark as primary key, second @ to make it auto increments
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
