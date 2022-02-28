3b8d0de7a85e6c56751349b97ad4b5b9153124db
unknown <joao.fzm@hotmail.com>
2022-02-27 21:54:25 -0300

    Project created

---
+++ src/main/java/br/com/joaofzm15/crud/WebServiceSpringBootHibernateCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaofzm15.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringBootHibernateCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSpringBootHibernateCrudApplication.class, args);
+	}
+
+}
