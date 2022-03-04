05bbabb72874574be715fcaaded9edd7ff66f275
unknown <joao.fzm@hotmail.com>
2022-03-03 22:37:26 -0300

    Create user service layers and component registration

--- src/main/java/br/com/joaofzm15/crud/repositories/UserRepository.java
+++ src/main/java/br/com/joaofzm15/crud/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.joaofzm15.crud.entities.User;
 
+//No need to @Repository, since this class extends JpaRepository, which is already @Repository
+// This class has methods like findAll and findById that it gets from it's super class
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
---
+++ src/main/java/br/com/joaofzm15/crud/services/UserService.java
@@ -0,0 +1,26 @@
+package br.com.joaofzm15.crud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.joaofzm15.crud.entities.User;
+import br.com.joaofzm15.crud.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
