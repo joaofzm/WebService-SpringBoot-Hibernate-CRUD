3c975ab5b99effe5db965b31edd54d86975bebf5
unknown <joao.fzm@hotmail.com>
2022-03-04 00:03:42 -0300

    Create Order Entity Class and it's associations with User class

--- src/main/java/br/com/joaofzm15/crud/entities/User.java
+++ src/main/java/br/com/joaofzm15/crud/entities/User.java
@@ -1,15 +1,20 @@
 package br.com.joaofzm15.crud.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name="user_table")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -23,6 +28,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> listOfOrders = new ArrayList<>();
+
 	public User() {
 		
 	}
@@ -34,6 +42,7 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
+	
 
 	public Long getId() {
 		return id;
@@ -73,6 +82,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getListOfOrders() {
+		return listOfOrders;
 	}
 
 	@Override
